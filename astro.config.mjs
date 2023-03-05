5a7b34cfb1e60dab2f9d5171e5cf5af121f71d94
Kartik <kartik.c.kulloli@gmail.com>
2023-03-05 09:33:00 +0530

    Full screen Feature + Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jeemainstimer.pages.dev/',
 	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: cloudflare()
 });
