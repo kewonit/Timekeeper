989a5f540956bba2087b36729c8d959207fb1f2b
Kartik <kartik.c.kulloli@gmail.com>
2023-03-19 10:37:53 +0530

    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   output: "server",
   adapter: cloudflare()
 });
